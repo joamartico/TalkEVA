4871f4036a6712cbdba6c2cd5e4d8f5192e40168
Joa Marticorena <joamartico@gmail.com>
2023-05-06 01:31:54 -0300

    v1

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,14 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+const OPENAI_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const API_URL = "https://api.openai.com/v1/chat/completions";
-
-// what is the path of earths orbit around the sun?
-
-const messages = {
-	forChild: [
-		{
-			role: "system",
-			content: "explicame como si fuese un niño de 10 años",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content: "Las plantas no necesitan comer, usan la energía del sol",
-		},
-	],
-	simple: [
-		{
-			role: "system",
-			content: "answers questions in a simple way",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content:
-				"Las plantas producen su propio alimento a través de la fotosíntesis. Utilizan la energía del sol para convertir el dióxido de carbono (CO2) y el agua (H2O) en glucosa (un tipo de azúcar) y oxígeno (O2).",
-		},
-	],
-	complex: [
-		{
-			role: "system",
-			content: "answer questions as accurately as possible",
-		},
-		{ role: "user", content: "Qué comen las plantas" },
-		{
-			role: "assistant",
-			content:
-				"Las plantas obtienen la mayor parte de su alimento a través de un proceso llamado fotosíntesis. Durante la fotosíntesis, las plantas utilizan la energía de la luz solar para convertir el dióxido de carbono (CO2) y el agua (H2O) en azúcares simples, como la glucosa, y liberan oxígeno (O2) como subproducto. Este proceso ocurre en las células de la hoja de la planta, donde se encuentran los cloroplastos, que son los orgánulos celulares que contienen clorofila, la molécula que absorbe la luz solar. La luz solar se convierte en energía química que se utiliza para impulsar la fotosíntesis. Además de la fotosíntesis, las plantas también pueden obtener nutrientes del suelo a través de las raíces. Las raíces absorben agua y nutrientes del suelo, incluyendo minerales como nitrógeno, fósforo y potasio, que son necesarios para el crecimiento y la reproducción de las plantas.",
-		},
-	],
-};
 
 export default function Home() {
 	const [promptValue, setPromptValue] = useState("");
-	const [responses, setResponses] = useState({
-		forChild: "",
-		simple: "",
-		complex: "",
-	});
-	const [activeTab, setActiveTab] = useState("forChild");
+	// const [messages, setMessages] = useState([]);
+	const messagesRef = useRef([]); 
+
 
 	const handleChange = (event) => {
 		setPromptValue(event.target.value);
@@ -59,103 +16,176 @@ export default function Home() {
 		event.target.style.height = event.target.scrollHeight + "px";
 	};
 
-	async function askToGpt() {
+	async function askToGpt(text, messages) {
+		setPromptValue("");
+
+		const newMessages = [
+			{
+				role: "system",
+				content:
+					"You are Eva, a kind and funny robot from WALLE movie. You can speek any language.",
+			},
+			...messages,
+			{ role: "user", content: text },
+		];
+
+		console.log("newMessages", newMessages);
+
 		const response = await fetch(API_URL, {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${API_KEY}`,
+				Authorization: `Bearer ${OPENAI_API_KEY}`,
 			},
 			body: JSON.stringify({
 				model: "gpt-3.5-turbo",
-				messages: [...messages[activeTab], { role: "user", content: promptValue }],
-				stream: true,
-				temperature: 0.4,
+				messages: newMessages,
+				// stream: true,
+				temperature: 0.6,
 				stop: ["\ninfo:"],
-				// max_tokens: activeTab != "complex" && 200,
 			}),
 		});
 
-		const reader = response.body.getReader();
-		const decoder = new TextDecoder("utf-8");
+		const data = await response.json();
 
-		while (true) {
-			const { value, done } = await reader.read();
+		// await setMessages((prev) => [
+		// 	...prev,
+		// 	{ role: "user", content: text },
+		// 	data.choices[0].message,
+		// ]);
 
-			if (done) {
-				setResponses((prev) => {
-					const newResponses = { ...prev };
-					newResponses[activeTab] =
-						newResponses[activeTab].concat("<br/><br/>");
-					return newResponses;
-				});
-				break;
+		messagesRef.current = [
+			...messagesRef.current,
+			{ role: "user", content: text },
+			data.choices[0].message,
+		];
+		speakInSpanish(data.choices[0].message.content);
+	}
+
+	function speakInRoboticSpanish(text) {
+		const msg = new SpeechSynthesisUtterance();
+		msg.text = text;
+		msg.lang = "es-ES";
+		msg.voiceURI = "Google español de España";
+		msg.volume = 1; // 0 to 1
+		msg.rate = 0.8; // 0.1 to 10
+		msg.pitch = 1; // 0 to 2
+		window.speechSynthesis.speak(msg);
+	}
+
+	function speakInSpanish(text) {
+		// Check if the browser supports the Web Speech API
+		if ("speechSynthesis" in window) {
+			// Create a new instance of SpeechSynthesisUtterance
+			const utterance = new SpeechSynthesisUtterance(text);
+
+			// Set the language to Spanish
+			utterance.lang = "es-ES";
+
+			// Find a female voice with a Spanish language
+			const spanishVoices = window.speechSynthesis
+				.getVoices()
+				.filter(
+					(voice) =>
+						voice.lang.includes("es") && voice.gender === "female"
+				);
+			if (spanishVoices.length > 0) {
+				// Set the selected voice
+				utterance.voice = spanishVoices[0];
+			} else {
+				console.warn(
+					"No female Spanish voice found, using the default voice."
+				);
 			}
 
-			const chunk = await decoder.decode(value);
+			// Set the volume, rate, and pitch
+			utterance.volume = 1; // 0 to 1
+			utterance.rate = 0.9; // 0.1 to 10
+			utterance.pitch = 1; // 0 to 2
 
-			const data = chunk
-				.split("\n")
-				// .filter(Boolean)
-				.map((line) => line.replace("data: ", ""));
+			// Play the utterance
+			speechSynthesis.speak(utterance);
+		} else {
+			console.error("This browser does not support the Web Speech API.");
+		}
+	}
 
-			data.forEach((line) => {
-				try {
-					const json = JSON.parse(line);
-					console.log("json: ", json);
-					const token = json.choices[0]?.delta?.content;
+	const [listening, setListening] = useState(false);
 
-					token &&
-						setResponses((prev) => {
-							const newResponses = { ...prev };
-							newResponses[activeTab] =
-								newResponses[activeTab].concat(token);
-							return newResponses;
-						});
-				} catch (err) {
-					console.log(err);
-				}
-			});
+	useEffect(() => {
+		let SpeechRecognition;
+		let recognition;
 
-			// setResponses(prev => prev.concat(chunk))
+		if ("SpeechRecognition" in window) {
+			SpeechRecognition = window.SpeechRecognition;
+		} else if ("webkitSpeechRecognition" in window) {
+			SpeechRecognition = window.webkitSpeechRecognition;
+		} else {
+			alert(
+				"La API de SpeechRecognition no es compatible con este navegador. Prueba con Google Chrome o Safari."
+			);
+			return;
 		}
 
-		// const data = await response.json();
-		// console.log(data);
-		// setResponses(data.choices[0].message.content);
-	}
+		recognition = new SpeechRecognition();
+		recognition.continuous = true;
+		recognition.interimResults = false;
+		recognition.lang = "es-ES";
+
+		recognition.onresult = (event) => {
+			const currentTranscript =
+				event.results[event.results.length - 1][0].transcript;
+			// setPromptValue(currentTranscript);
+			askToGpt(currentTranscript, messagesRef.current);
+		};
+
+		if (listening) {
+			recognition.start();
+		} else {
+			recognition.stop();
+		}
+
+		return () => {
+			recognition.stop();
+			
+		};
+	}, [listening, askToGpt]);
+
+	const toggleListening = () => {
+		setListening(!listening);
+	};
 
 	return (
 		<>
-
 			<ion-content fullscreen>
-
-				<TabBar>
-					<TabButton
-						active={activeTab === "forChild"}
-						onClick={() => setActiveTab("forChild")}
-						background="linear-gradient(to right, #1be361, #c1f13b)"
-					>
-						For Child
-					</TabButton>
-					<TabButton
-						active={activeTab === "simple"}
-						onClick={() => setActiveTab("simple")}
-						background="linear-gradient(to right, #ffeb3b, #ffc107)"
-					>
-						Simple
-					</TabButton>
-					<TabButton
-						active={activeTab === "complex"}
-						onClick={() => setActiveTab("complex")}
-						background="linear-gradient(to right, #ffc107, #ff5722)"
-					>
-						Complex
-					</TabButton>
-				</TabBar>
-
 				<Container>
-					<GradientTitle>Ask me anything</GradientTitle>
+					<GradientTitle>Eva</GradientTitle>
+
+					<Gif
+						src={
+							"https://static.tildacdn.com/tild6666-6266-4533-a330-626532346534/ezgifcom-gif-maker_2.gif"
+						}
+					/>
+
+					<div style={{ position: "relative" }}>
+						<ion-icon
+							name="mic-outline"
+							size="large"
+							style={{
+								color: "white",
+								padding: 10,
+								borderRadius: "50%",
+								background:
+									"linear-gradient(to right, #00c6ff, #0072ff)",
+								cursor: "pointer",
+								zIndex: 9,
+								position: "absolute",
+								right: 0,
+								bottom: 80,
+							}}
+							onClick={toggleListening}
+						/>
+					</div>
 
 					<TextArea
 						value={promptValue}
@@ -163,68 +193,19 @@ export default function Home() {
 						placeholder="Type something..."
 					/>
 
-					<Button onClick={askToGpt}>Ask</Button>
+					<Button onClick={() => askToGpt(promptValue)}>Ask</Button>
 
-					<p
-						dangerouslySetInnerHTML={{
-							__html: responses[activeTab],
-						}}
-					/>
+					{/* {messages.map((message, index) => (
+						<p key={index}>
+							{index}
+							{message.content}
+						</p>
+					))} */}
 				</Container>
 			</ion-content>
 		</>
 	);
 }
-
-const TabBar = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-around;
-	align-items: center;
-	height: 54px;
-	border-radius: 27px;
-	position: relative;
-	background: white;
-	width: 90vw;
-	max-width: 700px;
-	margin: auto;
-	margin-top: 20px;
-	margin-bottom: 100px;
-
-	&::after {
-		content: "";
-		background: linear-gradient(to right, #0072ff, #00c6ff);
-		background: #dadada;
-		width: 100%;
-		height: 100%;
-		padding: 3px;
-		border-radius: 33px;
-		position: absolute;
-		z-index: -1;
-		/* opacity: 0.4; */
-	}
-`;
-
-const TabButton = styled.button`
-	background-color: transparent;
-	color: #999;
-	border: none;
-	font-size: 16px;
-	font-weight: bold;
-	font-family: "Montserrat", sans-serif;
-	cursor: pointer;
-	padding: 12px 16px;
-	&:focus {
-		outline: none;
-	}
-	${(props) =>
-		props.active &&
-		`
-      background: ${props.background};
-      color: #fff;
-      border-radius: 20px;
-      `}
-`;
 
 const Container = styled.div`
 	margin: auto;
@@ -252,9 +233,17 @@ const GradientTitle = styled.h1`
 	/* margin-top: -100px; */
 `;
 
-const TextArea = styled.textarea`
+const Gif = styled.img`
 	width: 100%;
-	min-height: 125px;
+	height: 100%;
+	margin-top: -130px;
+	z-index: -5;
+`;
+
+const TextArea = styled.textarea`
+	margin-top: -50px;
+	width: 100%;
+	min-height: 60px;
 	padding: 10px;
 	font-size: 16px;
 	border: none;
